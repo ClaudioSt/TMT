d0b056ed0dd31610ef4c80545e4ab91e700c39a1
Clemens <clemens.maier@googlemail.com>
2017-07-23 14:14:51 +0200

    test draw circle

--- app/src/main/java/de/uni_stuttgart/projektinf/tmt/activities/TMTActivity.java
+++ app/src/main/java/de/uni_stuttgart/projektinf/tmt/activities/TMTActivity.java
@@ -4,18 +4,26 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import de.uni_stuttgart.projektinf.tmt.R;
+import de.uni_stuttgart.projektinf.tmt.classes.Circle;
 
 public class TMTActivity extends AppCompatActivity {
+    int numberOfCircles = 8;
+    int oldRandX = 0;
+    int oldRandY = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new TMTView(this));
+        // initialize circle array:
+
     }
 
     public class TMTView extends View {
@@ -27,18 +35,43 @@ public class TMTActivity extends AppCompatActivity {
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-            int x = getWidth();
-            int y = getHeight();
-            int radius;
-            radius = 100;
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.WHITE);
-            canvas.drawPaint(paint);
-            // Use Color.parseColor to define HTML colors
-            paint.setColor(Color.GREEN);
-            canvas.drawCircle(x / 2, y / 2, radius, paint);
+            drawTMT(canvas);
         }
+
+    }
+
+    private void calculatePositions(){
+        int randX = 0;
+        int randY = 0;
+        // zufällige Position (min 30px von alter Pos) bestimmen:
+        boolean foundPos = false;
+        while (!foundPos)
+        {
+            randX = (int)( Math.random() * (1024 - 2*Circle.RADIUS) ) + Circle.RADIUS;
+            randY = (int)( Math.random() * (768 - 2*Circle.RADIUS) ) + Circle.RADIUS;
+
+            //testen ob Entfernung zu voriger Position passt (euklidischer Abstand):
+            int abstand = (int) Math.sqrt( (oldRandX - randX)*(oldRandX - randX) + (oldRandY - randY)*(oldRandY - randY) );
+            if (abstand >= 30)
+            {
+                foundPos = true;
+            }
+        }
+        oldRandX = randX;
+        oldRandY = randY;
+
+    }
+
+    private void drawTMT(Canvas canvas){
+        int x = canvas.getWidth();
+        int y = canvas.getHeight();
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.WHITE);
+        canvas.drawPaint(paint);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(4.5f);
+        canvas.drawCircle(x / 2, y / 2, Circle.RADIUS, paint);
     }
 
 }
---
+++ app/src/main/java/de/uni_stuttgart/projektinf/tmt/classes/Circle.java
@@ -0,0 +1,9 @@
+package de.uni_stuttgart.projektinf.tmt.classes;
+
+/**
+ * Created by Clemens on 23.07.2017.
+ */
+
+public class Circle {
+    public static final int RADIUS = 100;
+}
